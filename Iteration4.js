d1bf0072666235f7a51d00946c7d3f19df1eeffd
Sergio Rodriguez <sergio.rolo94@gmail.com>
2021-10-10 18:56:30 +0200

    Iteracion 4

--- Iteration4.js
+++ Iteration4.js
@@ -1,4 +1,4 @@
-//! Iteración #4: Métodos findArrayIndex
+//!  Iteración #4: Métodos findArrayIndex
 
 // Crea una función llamada `findArrayIndex` que reciba como parametros un array de textos y un texto y devuelve la posición del array cuando el valor del array sea igual al valor del texto que enviaste como parametro. Haz varios ejemplos y compruebalos.
 
