879a23430cb4bb3e3282a8fedd1988858b4f097b
Sergio Rodriguez <sergio.rolo94@gmail.com>
2021-10-10 18:55:41 +0200

    Iteracion 3

--- Iteration3.js
+++ Iteration3.js
@@ -1,4 +1,4 @@
-//! Iteración #3: Mix Fors
+//!  Iteración #3: Mix Fors
 
 // Dado el siguiente javascript usa forof y forin para saber cuantas veces ha sido cada sonido agregado por los usuarios a favorito. Para ello recorre la lista de usuarios y usa forin para recoger el nombre de los sonidos que el usuario tenga como favoritos.
 // Una vez accedas a ellos piensa en la mejor forma de hacer un conteo de cada vez que ese sonido se repita como favorito en cada usuario. 
