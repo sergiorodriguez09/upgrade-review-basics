1b4ee30c68a6d0461bf12e4176b86f3cf14af230
Sergio Rodriguez <sergio.rolo94@gmail.com>
2021-10-10 18:57:28 +0200

    Iteracion 6

--- Iteration6.js
+++ Iteration6.js
@@ -1,4 +1,4 @@
-//! Iteración #6: Función swap
+//!  Iteración #6: Función swap
 
 // Crea una función llamada `swap()` que reciba un array y dos parametros que sean indices del array. La función deberá intercambiar la posición de los valores de los indices que hayamos enviado como parametro. Retorna el array resultante.
 
